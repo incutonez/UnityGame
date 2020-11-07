02055098e7a7c72b75686dc6a8030b8e99a3d50d
Jef <incutonez@hotmail.com>
2020-11-07 01:56:21 -0700

    - Initial add of character walking and jumping on platform

---
+++ Sandbox/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Because this script is attached to the camera object, the
+        // transform in this scope references the camera's transform
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called just like Update, but it runs after all
+    // other Updates have been processed... this ensures that when
+    // we set the position of the camera, we know the player has already
+    // moved to that frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Sandbox/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Diagnostics = System.Diagnostics;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jump = 400f;
+    public bool isGrounded = true;
+    public bool isFacedRight = false;
+    public float distanceToGround = 0f;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    private BoxCollider2D collider;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        //collider = GetComponent<BoxCollider2D>();
+        //distanceToGround = collider.bounds.extents.y;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distanceToGround + 0.1f);
+    }
+    void FixedUpdate()
+    {
+        // Grabs input from player on keyboard
+        // Keys are set by default in Input Manager
+        float moveX = Input.GetAxis("Horizontal") * speed;
+        float moveY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(0f, 0f);
+        if (isGrounded)
+        {
+            movement = new Vector2(moveX, moveY);
+            // +x is to the right
+            if (moveX > 0 && !isFacedRight || moveX < 0 && isFacedRight)
+            {
+                ChangeDirection();
+            }
+        }
+        if (moveY > 0 && isGrounded)
+        {
+            isGrounded = false;
+            moveY = jump;
+            movement = new Vector2(0f, moveY);
+            anim.SetBool("Ground", false);
+        }
+        /* Had to change Edit > Project Settings > Player > Other Settings
+         * > Active Input Handling to "Both" to be able to use this
+         * per https://forum.unity.com/threads/unity-render-pipeline-debug-clashes-with-new-input-system.735179/ */
+        rb2d.AddForce(new Vector2(moveX, moveY));
+        anim.SetFloat("Speed", Mathf.Abs(moveX));
+        anim.SetFloat("vSpeed", Mathf.Abs(moveY));
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //Diagnostics.Debug.WriteLine("OnCollisionStay2D");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
+    private void ChangeDirection()
+    {
+        isFacedRight = !isFacedRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
